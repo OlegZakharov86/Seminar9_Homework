59759e75be41397859a6f862e08cec9729181be5
OlegZakharov <varahas@mail.ru>
2022-08-08 15:24:55 +0300

    added Homework64

---
+++ Homework64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+namespace Seminar9
+{
+    class Homework66
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите первое значение M");
+            int M = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите первое значение N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = SumOfElements(M,N);
+            Console.WriteLine($"{sum}");
+        }
+        static int SumOfElements(int M, int N)
+        {
+            int sum = 0;
+            for (int i = M; i <= N; i++)
+            {
+                sum +=i;
+            }
+            return sum; 
+        }
+    }
+}
