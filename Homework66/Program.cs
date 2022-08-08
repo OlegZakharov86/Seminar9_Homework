2b5df205833386b41238ad70c34803da5f3f0a55
OlegZakharov <varahas@mail.ru>
2022-08-08 15:26:37 +0300

    added Homework66

---
+++ Homework66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+namespace Seminar9
+{
+    class Homework66
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите первое значение M");
+            int M = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите первое значение N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            string result = NewString(M, N);
+            Console.WriteLine($"{result}");
+        }
+        static string NewString(int M, int N)
+        {
+            string result = String.Empty;
+            for (int i = M; i <= N; i++)
+            {
+                result += $"{i} ";
+            }
+            return result;
+        }
+    }
+}
